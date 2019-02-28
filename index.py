82b723dac02c7fb197c33926c53b7687f549171d
Tulsani <38261746+Tulsani@users.noreply.github.com>
2019-03-01 04:21:06 +0530

    errors removed

--- index.py
+++ index.py
@@ -58,7 +58,7 @@ classfier.fit_generator(
 import numpy as np
 from keras.preprocessing import image
 test_image = image.load_img('dog.jpg',target_size=(64,64))
-test_image = image.image_to_array(test_image)
+test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image,axis=0)
 result = classfier.predict(test_image)
 training_set.class_indices
